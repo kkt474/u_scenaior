08d4a0c9ed47da688a6e6a54e6c6603e134fd8a7
Unknown <unknown@example.com>
2024-10-21 12:41:38 +0800

    update: add

--- include/hybrid_a_star.h
+++ include/hybrid_a_star.h
@@ -11,6 +11,23 @@ struct GridInfo{
     int theta_resolution;
 };
 
+struct GridPoint{
+    int x; 
+    int y;
+    int theta;
+    bool operator==(const GridPoint &other) const {
+        return (x == other.x && y == other.y);
+    }
+};
+
+// ??????????????????????????????????????????
+struct GridPointHasher{
+    std:;size_t operator()(const GridPoint &k) const
+    {
+        return ((std::hash<int>()(k.x) ^ (std::hash<int>()(k.y) << 1)) >> 1);
+    }
+};
+
 struct State{
     double x;
     double y;
@@ -24,9 +41,50 @@ struct SearchConfig{
     double time_step;
 };
 
+class StateHeap{
+public:
+    bool Empty(){
+        return pq.size();
+    }
+
+    size_t Size(){
+        return pq.size();
+    }
+
+    void AddState(const State &state){
+        pq.push(state);
+    }
+
+    State GetState(){
+        State state = pq.top();
+        pq.top();
+        return state;
+    }
+
+private:
+    class StateComparator{
+        public:
+           bool operator()(const State &x, const State &y){
+                   return x.estimate_cost > y.estimate_cost;
+           }
+    };
+
+    std::priority_queue<State, std::vector<State>, StateComparator> pq;    //////222222222222222222222222222222
+};
+
 class HybridAStarSolver{
+private:
+   enum class ExploreState{
+     UNEXPLORED = 0,
+     ON_open_set = 1;
+     EXPLORED = 2
+   };
 
 public:
+    using ExploreMap = std::vector<std::unordered_map<GridPoint, ExploreState, GridPointHasher>>;
+
+    using StateMap = std::vector<std::unordered_map<GridPoint, State, GridPointHasher>>;
+
     HybridAStarSolver(const GridInfo &map_info, const MotionConfig &motion_config);
 
     std::vector<State> Solve(const Point &start, const Point &goal, bool *goal_reached);
--- src/hybrid_a_star.cpp
+++ src/hybrid_a_star.cpp
@@ -1,4 +1,5 @@
 #include "hybrid_a_star.h"
+#include <cmath>
 
 namespace U_turn_scenario{
 namespace hybrid_a_star{
@@ -7,8 +8,13 @@ HybridAStarSolver::HybridAStarSolver(const GridInfo &map_info, const MotionConfi
       map_info_(map_info),
       motion_config_(motion_config)
       {
-
+       /**/
       }  
+
+double HybridAStarSolver::Distance(double x1, double y1, double x2, double y2)
+{
+      return std::sqrt(std::pow(x1-x2,2)+std::pow(y1-y2,2));
+}
 
 // 搜索主函数
 std::vector<State> HybridAStarSolver::Solve(const Point &start, const Point &goal,
@@ -16,6 +22,25 @@ std::vector<State> HybridAStarSolver::Solve(const Point &start, const Point &goa
       start_ = start;
       goal_  = goal;
       theta_start_to_goal_ = std::atan2(goal.y - start.y, goal.x - start.x);
+      distance_start_to_goal_ = Distance(star.x, start.y, goal.x, goal.y);
+
+      // explored configuration space
+      ExploreMap explored;
+      explored.resize(map_info_.theta_resolution);
+      // parent state for reconstruction
+      StateMap parent;
+      parent.resize(map_info_.theta_resolution);
+
+      // open_set
+      StateHeap open_set;
+      State init;
+      init.x = start.x;
+      init.y = start.y;
+      init.theta = start.theta;
+      init.actual_cost = 0.;
+      init.estimated_cost = 0.;
+
+
 
 }
 
